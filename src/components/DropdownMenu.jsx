6fcdb77b6ffd1c706c8aaaeaad333f166ed2b3ee
ekaterinakutovaya <e.katerina.kutovaya@yandex.ru>
2023-05-12 17:36:34 +0500

    burger button fix

--- src/components/DropdownMenu.jsx
+++ src/components/DropdownMenu.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Link from "next/link";
+import { motion } from "framer-motion";
 
 import Figure from "assets/icons/Figure";
 import { Accordeon, SocialBar } from "components";
@@ -14,30 +15,32 @@ const DropdownMenu = () => {
   ];
 
   return (
-    <nav className="w-full h-[calc(100vh_-_78px)] bg-white absolute top-[78px] z-50 border-solid border-t border-t-[#ECECEC]">
-      <ul className="w-full p-7 flex flex-col gap-y-8 ">
-        <li>
-          <Accordeon label={"Услуги"} items={nestedList} />
-        </li>
+    // <motion.div initial={{ height: 0 }} animate={{ height: 300 }}>
+      <nav className="w-full h-[calc(100vh_-_78px)] bg-white absolute top-[78px] z-50 border-solid border-t border-t-[#ECECEC]">
+        <ul className="w-full p-7 flex flex-col gap-y-8 ">
+          <li>
+            <Accordeon label={"Услуги"} items={nestedList} />
+          </li>
 
-        {navigation &&
-          navigation.map((item, index) => (
-            <li className="font-arimo font-bold text-[20px]">
-              <Link href={item.path}>{item.title}</Link>
-            </li>
-          ))}
+          {navigation &&
+            navigation.map((item, index) => (
+              <li className="font-arimo font-bold text-[20px]">
+                <Link href={item.path}>{item.title}</Link>
+              </li>
+            ))}
 
-        <li className="mt-[40px]">
-          <div className="font-arimo font-bold text-[18px] pb-[20px]">
-            Следите в телеграмме
-          </div>
+          <li className="mt-[40px]">
+            <div className="font-arimo font-bold text-[18px] pb-[20px]">
+              Следите в телеграмме
+            </div>
 
-          <SocialBar color="#1B2330" size="40" />
-        </li>
-      </ul>
+            <SocialBar color="#1B2330" size="40" />
+          </li>
+        </ul>
 
-      <Figure className="absolute bottom-[20%] right-[-8%]" />
-    </nav>
+        <Figure className="absolute bottom-[20%] right-[-8%]" />
+      </nav>
+    // </motion.div>
   );
 };
 
--- src/components/Header/HeaderMobile.jsx
+++ src/components/Header/HeaderMobile.jsx
@@ -38,7 +38,7 @@ const HeaderMobile = () => {
           </div>
 
           <div className="flex items-center sm:gap-x-[10px] lg:none">
-            <div className="w-10 h-10 border border-solid border-lightGrey rounded-full flex justify-center items-center">
+            <div className="w-[45px] h-[45px] border border-solid border-lightGrey rounded-full flex justify-center items-center">
               <BiGlobe className="fill-grey" />
             </div>
 
@@ -56,23 +56,25 @@ const HeaderMobile = () => {
             </button>
           </div>
 
-          <ul className="hidden lg:flex item-center gap-x-[30px]">
-            {navigation &&
-              navigation.map((item, index) => (
-                <li
-                  key={index}
-                  className="flex items-center ease-in-out duration-300 hover:text-[#28C79E]"
-                >
-                  <Link href={item.path}>{item.title}</Link>
-                </li>
-              ))}
+          
+            <ul className="hidden lg:flex item-center gap-x-[30px]">
+              {navigation &&
+                navigation.map((item, index) => (
+                  <li
+                    key={index}
+                    className="flex items-center ease-in-out duration-300 hover:text-[#28C79E]"
+                  >
+                    <Link href={item.path}>{item.title}</Link>
+                  </li>
+                ))}
 
-            <li className="language-toggler flex justify-between items-center">
-              <span className="ru cursor-pointer">Ru</span>
-              <span className="mx-[8px] w-[1px] h-[16px] bg-white"></span>
-              <span className="uz text-[#DBDBDB] cursor-pointer">Uz</span>
-            </li>
-          </ul>
+              <li className="language-toggler flex justify-between items-center">
+                <span className="ru cursor-pointer">Ru</span>
+                <span className="mx-[8px] w-[1px] h-[16px] bg-white"></span>
+                <span className="uz text-[#DBDBDB] cursor-pointer">Uz</span>
+              </li>
+            </ul>
+          
         </div>
 
         {openMenu && <DropdownMenu />}
